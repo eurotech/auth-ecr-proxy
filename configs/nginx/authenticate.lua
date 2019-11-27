9d068ae8215a1b5999f61df49252237169dd373d
Denni Albiani <denni.albiani@eurotech.com>
2019-11-27 11:53:51 +0100

    Fix authentication for docker proxy

--- configs/nginx/authenticate.lua
+++ configs/nginx/authenticate.lua
@@ -18,6 +18,18 @@ if ngx.var.http_authorization then
     remote_password = tmp:sub(tmp:find(':')+1)
 end
  
+function split(inputstr, sep)
+    if sep == nil then
+            sep = "%s"
+    end
+    local t={} ; local i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+            t[i] = str
+            i = i + 1
+    end
+    return t
+end
+
 function authentication_prompt()
     session.data.valid_user = false
     session.data.user_group = nil
@@ -52,15 +64,25 @@ function authenticate(user, password, uri)
  
     user = ngx.quote_sql_str(user)
     password = ngx.quote_sql_str(password)
-    uri = ngx.quote_sql_str(uri)
-    local query = "select 1 from users us INNER JOIN groups_users_ref gr_us on us.id_us = gr_us.id_us INNER JOIN groups gr on gr_us.id_gr = gr.id_gr INNER JOIN groups_uri_ref gr_ur on gr.id_gr = gr_ur.id_gr INNER JOIN uri ur on gr_ur.id_ur = ur.id_ur WHERE username = %s and password = MD5(%s) and uri = %s;"
+
+    if  uri == "/v2/" then
+        uri = ngx.quote_sql_str(uri)
+        query = "select 1 from users us INNER JOIN groups_users_ref gr_us on us.id_us = gr_us.id_us INNER JOIN groups gr on gr_us.id_gr = gr.id_gr INNER JOIN groups_uri_ref gr_ur on gr.id_gr = gr_ur.id_gr INNER JOIN uri ur on gr_ur.id_ur = ur.id_ur WHERE username = %s and password = MD5(%s) and uri = %s;"
+    else
+        splituri = split(uri, "/")
+        image = splituri[3]
+        tag = splituri[5]
+        uri = "\'%" .. image .. "%" .. tag .. "%\'"
+        query = "select 1 from users us INNER JOIN groups_users_ref gr_us on us.id_us = gr_us.id_us INNER JOIN groups gr on gr_us.id_gr = gr.id_gr INNER JOIN groups_uri_ref gr_ur on gr.id_gr = gr_ur.id_gr INNER JOIN uri ur on gr_ur.id_ur = ur.id_ur WHERE username = %s and password = MD5(%s) and uri like %s;"
+    end
+
     query = string.format(query, user, password, uri);
     res, err, errno, sqlstate = db:query(query)
  
     if res and res[1] then
         session.data.valid_user = true
         session:open()
-    elseif ( string.find(uri, "blobs") or string.find(uri, "manifests") )then
+    elseif string.find(uri, "sha256") then
         session.data.valid_user = true
         session:open()
     else
@@ -104,9 +126,9 @@ if session.present and (session.data.valid_user) then
     return
 elseif ngx.var.remote_user and remote_password then
     authenticate(ngx.var.remote_user, remote_password, ngx.var.request_uri)
-    if ( not string.find(ngx.var.request_uri, "blobs") or not string.find(ngx.var.request_uri, "manifests") ) then
+    if ( not string.find(ngx.var.request_uri, "blobs") ) then
     log_access(ngx.var.remote_user, ngx.var.request_uri)
     end
 else
     authentication_prompt()
-end 
+end
